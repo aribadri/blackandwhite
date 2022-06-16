83cf712cbda6744c6f7db34c33f7d0e6bc6d8161
aribadri <aribad101@icloud.com>
2022-06-16 13:57:52 +0300

    routing preinstall

---
+++ README.md
@@ -0,0 +1,5 @@
+# blackandwhite
+
+NFT marketplace
+
+here you can raise ez money and release from frontend burden xD
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import  Layout  from './components/Layout/Layout.jsx'
 
 function App() {
   return (
     <div className="App">
-     
+        <Routes>
+        <Route path="/" element={<Layout/>}/>
+     </Routes>
+
     </div>
   );
 }
